a0cc3e6f4b574ece8d00cd47e688a3e39ee8d754
eaverdeja <eaverdeja@gmail.com>
2025-02-02 17:54:38 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,11 +8,17 @@ def _process_connection(conn: socket.SocketType):
     # https://kafka.apache.org/protocol.html#protocol_messages
     # First 4 bytes are the message size
     _message_size = int.from_bytes(request[:4])
+
     # Next 2 bytes are the request API key
+    # A Kafka request specifies the API its calling by using the request_api_key header field.
     _request_api_key = int.from_bytes(request[4:6])
+
     # Next 2 bytes are the request API version
+    # Requests use the header field request_api_version to specify the API version being requested.
     _request_api_version = int.from_bytes(request[6:8])
+
     # Finally, the next 4 bytes are the correlation_id
+    # This field lets clients match responses to their original requests
     correlation_id = int.from_bytes(request[8:12])
 
     # Keep the length hardcoded for now
@@ -20,7 +26,10 @@ def _process_connection(conn: socket.SocketType):
     # Response Header v0
     header = correlation_id.to_bytes(length=4)
 
-    response = length + header
+    error_code = int(35).to_bytes(length=2)
+    body = error_code
+
+    response = length + header + body
     conn.send(response)
     conn.close()
 
